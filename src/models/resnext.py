d8b52963b3613ede2fa96f33217e0ff8d49a2254
sarah <sarah@mikami.ch>
2025-04-29 18:34:53 +0200

    basic vision models layers

--- src/models/resnext.py
+++ src/models/resnext.py
@@ -13,5 +13,9 @@ class ResNeXt(IModel):
         """
         Returns the layers on which to do the linear probing.
         """
-        module = self.model.get_submodule("layer4")
-        return [('layer4', module)]
+        
+        layer3 = self.model.get_submodule("layer3")
+        layer4 = self.model.get_submodule("layer4")
+        avgpool = self.model.get_submodule("avgpool")
+        
+        return [('layer3', layer3), ('layer4', layer4), ('avgpool', avgpool)]
